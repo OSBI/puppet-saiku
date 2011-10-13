727878c8a64f3966eb49d80aabbf23826681a0f0
Tom Barber <tom@analytical-labs.com>
2011-10-13 20:15:28 +0100

    changes

--- manifests/classes/saiku-apt.pp
+++ manifests/classes/saiku-apt.pp
@@ -2,11 +2,11 @@ class saiku::apt {
 include apt
 
         apt::key { "alabs repo":
-                source => "http://repo.analytical-labs.com/public.key"
+                source => "http://debian.analytical-labs.com/public.key"
         }
 
         apt::sources_list { "analyticallabs":
-                content => "deb http://repo.analytical-labs.com/ natty main"
+                content => "deb http://debian.analytical-labs.com/ natty main"
         }
 
 
