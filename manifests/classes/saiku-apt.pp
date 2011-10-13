bce307ce7be8ea9b7248fe9b7cc8f33090026103
Tom Barber <tom@analytical-labs.com>
2011-10-13 19:53:47 +0100

    change repo loc

--- manifests/classes/saiku-apt.pp
+++ manifests/classes/saiku-apt.pp
@@ -2,11 +2,11 @@ class saiku::apt {
 include apt
 
         apt::key { "alabs repo":
-                source => "http://webserver.analytical-labs.com/debian/public.key"
+                source => "http://repo.analytical-labs.com/public.key"
         }
 
         apt::sources_list { "analyticallabs":
-                content => "deb http://webserver.analytical-labs.com/debian natty main"
+                content => "deb http://repo.analytical-labs.com/ natty main"
         }
 
 
