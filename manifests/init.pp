f1ada60857a56a6ec58efbdc077ab7463d230b72
Tom Barber <tom@analytical-labs.com>
2011-10-13 17:13:58 +0100

    break out files

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,19 @@ import "classes/*.pp"
 
 class saiku {
 
-include saiku::webstack
-include saiku::apt
-include saiku::app
-include saiku::data	
+class { "saiku::webstack":}
+class { "saiku::apt":
+		requires => Class[saiku::webstack]
+	  }
+class { "saiku::app":
+		requires => Class[saiku::apt]
+		}
+class { "saiku::data":
+		requires => Class[saiku::app]}
+#include saiku::webstack
+#include saiku::apt
+#include saiku::app
+#include saiku::data	
 
 
 }
