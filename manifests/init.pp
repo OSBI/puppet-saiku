b79af9fef14ffa2b16ce05e223b99a10f90d47a5
Tom Barber <tom@analytical-labs.com>
2011-10-13 16:25:58 +0100

    refactor code

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,15 +2,28 @@ import "classes/*.pp"
 
 class saiku {
 
-include tomcat
-
+include tomcat::source
 include saiku::data
+include apt
+	
+	apt::key { "alabs repo":
+		source => "http://webserver.analytical-labs.com/debian/public.key"
+	}
+
+	apt::sources_list { "analyticallabs":
+		content => "deb http://webserver.analytical-labs.com/debian natty main"
+	}
+	
+	tomcat::instance {"saiku":
+  		ensure      => present,
+  		ajp_port    => "8009",
+	}
 
 
     package { 'saiku':
         ensure => latest,
         notify  => Service['tomcat7'],
-	require => Package['tomcat7-alabs'],
+		require => Tomcat::Instance["saiku"],
     }
 
 
