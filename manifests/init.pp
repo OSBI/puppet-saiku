18387d6093cda4c04b5bbf73f021561830ca4e7a
Tom Barber <tom@analytical-labs.com>
2011-10-13 16:46:29 +0100

    break out files

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,29 +2,9 @@ import "classes/*.pp"
 
 class saiku {
 
-include tomcat::source
-include saiku::data
-include apt
-	
-	apt::key { "alabs repo":
-		source => "http://webserver.analytical-labs.com/debian/public.key"
-	}
-
-	apt::sources_list { "analyticallabs":
-		content => "deb http://webserver.analytical-labs.com/debian natty main"
-	}
-	
-	tomcat::instance {"saiku":
-  		ensure      => present,
-  		ajp_port    => "8009",
-	}
-
-
-    package { 'saiku':
-        ensure => latest,
-        #notify  => Service['tomcat-saiku'],
-		require => Tomcat::Instance["saiku"],
-    }
+include saiku::webstack
+include saiku::apt
+include saiku::app	
 
 
 }
