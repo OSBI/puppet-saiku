3e4a6b20a091b7b88b40eb966572e4c6c59d1117
Tom Barber <tom@analytical-labs.com>
2011-10-14 09:44:52 +0100

    changes

--- manifests/definitions/saikuinstance.pp
+++ manifests/definitions/saikuinstance.pp
@@ -1,23 +1,15 @@
 define saiku::instance($ensure , $tomcat_name, $tomcat_http, $tomcat_ajp, $tomcat_server, $app_name = 'saiku') {
 
 		include tomcat::source
-
+		include saiku::apt
+		include saiku::app
+		include saiku::data
         tomcat::instance {"${name}":
                 ensure      => present,
                 ajp_port    => "${tomcat_ajp}",
                 server_port    => "${tomcat_server}",
                 http_port    => "${tomcat_http}",
         }
-
-		class { "saiku::apt":
-			require => Tomcat::Instance["${name}"]
-	  	}
-		class { "saiku::app":
-			require => Class["saiku::apt"]
-		}
-		class { "saiku::data":
-			require => Class["saiku::app"]
-		}
 
 
 
