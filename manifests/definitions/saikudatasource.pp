7cdb1cd94df0a9d820a0f08ce547b099d5f17447
Tom Barber <tom@analytical-labs.com>
2011-10-31 21:41:15 +0000

    add data source

--- manifests/definitions/saikudatasource.pp
+++ manifests/definitions/saikudatasource.pp
@@ -1,7 +1,7 @@
 define saiku::datasource($ensure , $tomcat_name) {
   
   file { "/srv/tomcat/${tomcat_name}/webapps/saiku/WEB-INF/classes/saiku-datasources/${name}":
-      ensure => ${ensure},
+      ensure => $ensure,
    }
 
 }
