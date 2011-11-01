ecce989c59a8ba1e9d2f007f10d2b718a5e20fdb
Tom Barber <tom@analytical-labs.com>
2011-11-01 14:05:04 +0000

    fix template

--- manifests/definitions/saikudatasource.pp
+++ manifests/definitions/saikudatasource.pp
@@ -2,6 +2,7 @@ define saiku::datasource($ensure , $tomcat_name, $datasource_name) {
   
   file { "/srv/tomcat/${tomcat_name}/webapps/saiku/WEB-INF/classes/saiku-datasources/${datasource_name}":
       ensure => $ensure,
+      content => template('saiku/foodmart_mysql.erb')
    }
 
 }
