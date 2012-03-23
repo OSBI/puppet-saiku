ec74cf7fc75cd335bf26ba92dfbe7a3228cba6cb
Tom Barber <tom@analytical-labs.com>
2012-03-23 10:57:18 +0000

    change

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -38,7 +38,7 @@ define saiku::instance($ensure , $app_name = 'saiku') {
       ensure => present,
       datasource_name => "foodmart_mysql_${name}",
       tomcat_name => "${name}",
-      notify  => Service["tomcat-${name}"],
+      #notify  => Service["tomcat-${name}"],
      # require =>Package["${app_name}"],
     }
 
