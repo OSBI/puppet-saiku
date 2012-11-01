ca92e5c0582b472c79bfce09628707f44fe82248
Tom Barber <tom@analytical-labs.com>
2012-11-01 16:13:03 +0000

    refactor

--- manifests/server.pp
+++ manifests/server.pp
@@ -13,19 +13,19 @@ class saiku::server($app_name, $default_datasource, $database){
 #     command => "chown -R tomcat:tomcatshared /srv/tomcat/saiku/webapps/",
 # } ->
   saiku::datasource {
-    "foodmart_dev_${name}" :
+    "foodmart_dev_saiku" :
       ensure => absent,
       datasource_name => "foodmart",
       tomcat_name => "${name}",
       require =>Package["${app_name}"],
-      notify  => Service["tomcat-${name}"],
+      notify  => Service["tomcat-saiku"],
   } 
   if ($default_datasource == true) {
     saiku::datasource {
-      "foodmart_${database}_dev_${name}" :
+      "foodmart_${database}_dev_saiku" :
         ensure => present,
-        datasource_name => "foodmart_${database}_${name}",
-        tomcat_name => "${name}",
+        datasource_name => "foodmart_${database}_saiku",
+        tomcat_name => "saiku",
         notify  => Service["tomcat-saiku"],
         require =>Package["${app_name}"],
         database_type => $database,
